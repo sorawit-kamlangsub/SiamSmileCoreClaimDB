030d3c581e31eb8aecd98685aa963142cffcb5df
sorawot(kem) <sorawit.kam@gmail.com>
2026-08-28 10:49:56 +0700

    [CoreClaim] ปรับปรุง ทดสอบ สโตร์ Insert Case Adjustment

--- SiamSmileCoreClaim/InsertCaseAdjustment.sql
+++ SiamSmileCoreClaim/InsertCaseAdjustment.sql
@@ -7,6 +7,11 @@ GO
             ,@ClaimNo VARCHAR(50)
             ,@UserId INT
             ;
+    SET @UserId = 1;
+    SET @AdjustmentReasonId = 2;
+    SET @AdjustmentAmount = 1500;
+    SET @ClaimNo = 'CL690800000174';
+
 
 /*Default Declare*/
 	DECLARE @IsResult	BIT;
@@ -21,56 +26,42 @@ GO
 	IF @IsResult = 0 
 	BEGIN
 		SET @Msg = N'ปิดใช้งาน'
-		--GOTO RESULT;
+		GOTO RESULT;
 	END;
 
 /* Setup Data*/
     DECLARE @D2	DATETIME2(7);
     DECLARE @CaseAdjustmentId UNIQUEIDENTIFIER;
-    DECLARE @Tmp TABLE 
-    (
-     CaseId UNIQUEIDENTIFIER
-     ,CaseAdjudicationId UNIQUEIDENTIFIER
-     --,PayableCategoryId INT
-     --,PayeeTypeId INT
-     --,FromBankId VARBINARY(8000) 
-     --,FromBankName VARBINARY(8000) 
-     --,FromBankAccountNo VARBINARY(8000) 
-     --,ToBankId VARBINARY(8000) 
-     --,ToBankName VARBINARY(8000) 
-     ,ToBankAccountNo VARBINARY(8000) 
-    );
 
-	SET @D2 = GETDATE();
+    DECLARE
+         @CaseId                UNIQUEIDENTIFIER
+         ,@CaseAdjudicationId   UNIQUEIDENTIFIER
+         ,@PayableCategoryId    INT
+         ,@PayeeTypeId          INT
+         ,@FromBankId           INT 
+         ,@FromBankName         VARCHAR(50) 
+         ,@FromBankAccountNo    VARCHAR(50) 
+         ,@ToBankId             INT 
+         ,@ToBankName           VARCHAR(50)
+         ,@ToBankAccountNo      VARCHAR(50)
+         ,@CountValidate        INT ;
+
     SET @CaseAdjustmentId = NEWID();
-    SET @AdjustmentReasonId = 2;
-    SET @AdjustmentAmount = 1500;
-    SET @ClaimNo = 'CL690800000131';
+   	SET @D2 = GETDATE();
 
-    --INSERT INTO @Tmp
-    --(
-    -- CaseId
-    -- ,CaseAdjudicationId
-    -- --,PayableCategoryId
-    -- --,PayeeTypeId
-    -- --,FromBankId
-    -- --,FromBankName
-    -- --,FromBankAccountNo
-    -- --,ToBankId
-    -- --,ToBankName
-    -- ,ToBankAccountNo
-    --)
     SELECT 
-     [case].CaseId
-     ,payable.CaseAdjudicationId
-     --,payable.PayableCategoryId
-     --,payable.PayeeTypeId
-     --,payable.FromBankId
-     --,payable.FromBankName
-     --,payable.FromBankAccountNo
-     --,payable.ToBankId
-     --,payable.ToBankName
-     ,CAST(payable.ToBankAccountNo AS VARBINARY(8000)) ToBankAccountNo
+    @CaseId              = [case].CaseId
+    ,@CaseAdjudicationId = payable.CaseAdjudicationId
+    ,@PayableCategoryId  = payable.PayableCategoryId
+    ,@PayeeTypeId        = payable.PayeeTypeId
+    ,@FromBankId         = payable.FromBankId
+    ,@FromBankName       = payable.FromBankName
+    ,@FromBankAccountNo  = payable.FromBankAccountNo
+    ,@ToBankId           = payable.ToBankId
+    ,@ToBankName         = payable.ToBankName
+    ,@ToBankAccountNo    = payable.ToBankAccountNo
+    ,@CountValidate      = COUNT([case].CaseId) OVER()
+
     FROM core.[Case] [case]
     INNER JOIN core.Claim claim
         ON [case].ClaimId = claim.ClaimId
@@ -81,101 +72,105 @@ GO
     INNER JOIN finance.Payment pay
         ON payItem.PaymentId = pay.PaymentId
     WHERE pay.PaymentStatusId = 3
-    AND claim.ClaimNo = @ClaimNo
+    AND claim.ClaimNo = @ClaimNo;
 
 /* Validate Data */
+	IF @CountValidate > 1 
+	BEGIN
+		SET @Msg = N'มี CC มากกว่า 1 รายการ'
+		GOTO RESULT;
+	END;
 
 BEGIN TRY
 	BEGIN TRANSACTION
 
-    --INSERT INTO [process].[CasePayable]
-    --           ([CasePayableId]
-    --           ,[CaseId]
-    --           ,[CaseAdjudicationId]
-    --           ,[CaseAdjustmentId]
-    --           ,[PayableCategoryId]
-    --           ,[PayableStatusId]
-    --           ,[PayableAmount]
-    --           ,[TotalPaidAmount]
-    --           ,[OutstandingAmount]
-    --           ,[PayeeTypeId]
-    --           ,[FromBankId]
-    --           ,[FromBankName]
-    --           ,[FromBankAccountNo]
-    --           ,[ToBankId]
-    --           ,[ToBankName]
-    --           ,[ToBankAccountNo]
-    --           ,[IsActive]
-    --           ,[CreatedByUserId]
-    --           ,[CreatedDate]
-    --           ,[UpdatedByUserId]
-    --           ,[UpdatedDate])
+    INSERT INTO [process].[CaseAdjustment]
+    (
+        [CaseAdjustmentId],
+        [AdjustmentTypeId],
+        [CaseId],
+        [CaseAdjudicationId],
+        [AdjustmentDate],
+        [AdjustmentReason],
+        [AdjustmentAmount],
+        [IsActive],
+        [CreatedByUserId],
+        [CreatedDate],
+        [UpdatedByUserId],
+        [UpdatedDate]
+    )
+    SELECT
+        @CaseAdjustmentId       CaseAdjustmentId
+        ,2                      AdjustmentTypeId
+        ,@CaseId                CaseId
+        ,@CaseAdjudicationId    CaseAdjudicationId
+        ,@D2                    AdjustmentDate
+        ,N'ผู้ให้บริ '              AdjustmentReason
+        ,@AdjustmentAmount      AdjustmentAmount
+        ,1                      IsActive
+        ,@UserId                CreatedByUserId
+        ,@D2                    CreatedDate
+        ,@UserId                UpdatedByUserId
+        ,@D2                    UpdatedDate
+
+    INSERT INTO [process].[CasePayable]
+               ([CasePayableId]
+               ,[CaseId]
+               ,[CaseAdjudicationId]
+               ,[CaseAdjustmentId]
+               ,[PayableCategoryId]
+               ,[PayableStatusId]
+               ,[PayableAmount]
+               ,[TotalPaidAmount]
+               ,[OutstandingAmount]
+               ,[PayeeTypeId]
+               ,[FromBankId]
+               ,[FromBankName]
+               ,[FromBankAccountNo]
+               ,[ToBankId]
+               ,[ToBankName]
+               ,[ToBankAccountNo]
+               ,[IsActive]
+               ,[CreatedByUserId]
+               ,[CreatedDate]
+               ,[UpdatedByUserId]
+               ,[UpdatedDate])
     SELECT
      NEWID()                [CasePayableId]
-     ,[CaseId]
-     ,[CaseAdjudicationId]
+     ,@CaseId               CaseId
+     ,@CaseAdjudicationId   CaseAdjudicationId
      ,@CaseAdjustmentId     [CaseAdjustmentId]
-     ,[PayableCategoryId]
+     ,@PayableCategoryId    PayableCategoryId
      ,2                     [PayableStatusId]
      ,@AdjustmentAmount     [PayableAmount]
      ,@AdjustmentAmount     [TotalPaidAmount]
      ,@AdjustmentAmount     [OutstandingAmount]
-     ,[PayeeTypeId]
-     ,[FromBankId]
-     ,[FromBankName]
-     ,[FromBankAccountNo]
-     ,[ToBankId]
-     ,[ToBankName]
-     ,[ToBankAccountNo]
+     ,@PayeeTypeId          PayeeTypeId
+     ,@FromBankId           FromBankId
+     ,@FromBankName         FromBankName
+     ,@FromBankAccountNo    FromBankAccountNo
+     ,@ToBankId             ToBankId
+     ,@ToBankName           ToBankName
+     ,@ToBankAccountNo      ToBankAccountNo
      ,1                     [IsActive]
      ,@UserId               [CreatedByUserId]
      ,@D2                   [CreatedDate]
      ,@UserId               [UpdatedByUserId]
      ,@D2                   [UpdatedDate]
-    FROM @Tmp
-
-    --INSERT INTO [process].[CaseAdjustment]
-    --(
-    --    [CaseAdjustmentId],
-    --    [AdjustmentTypeId],
-    --    [CaseId],
-    --    [CaseAdjudicationId],
-    --    [AdjustmentDate],
-    --    [AdjustmentReason],
-    --    [AdjustmentAmount],
-    --    [IsActive],
-    --    [CreatedByUserId],
-    --    [CreatedDate],
-    --    [UpdatedByUserId],
-    --    [UpdatedDate]
-    --)
-    SELECT
-        @CaseAdjustmentId   CaseAdjustmentId
-        ,2                  AdjustmentTypeId
-        ,CaseId
-        ,CaseAdjudicationId CaseAdjudicationId
-        ,@D2                AdjustmentDate
-        ,N'ผู้ให้บริ '           AdjustmentReason
-        ,@AdjustmentAmount  AdjustmentAmount
-        ,1                  IsActive
-        ,1                  CreatedByUserId
-        ,@D2                CreatedDate
-        ,1                  UpdatedByUserId
-        ,@D2                UpdatedDate
-    FROM @Tmp ;
 
 	COMMIT TRANSACTION
 END TRY
 BEGIN CATCH
-
+    SET @IsResult = 0
+    SET @Msg = ERROR_MESSAGE()
 	IF @@TRANCOUNT > 0 ROLLBACK;
 END CATCH
 
 IF @IsResult = 1 BEGIN	SET @Result = 'Success' END	
 ELSE BEGIN				SET @Result = 'Failure' END;	
 
+RESULT:
+
 SELECT @IsResult IsResult
 		,@Result Result
 		,@Msg	 Msg;
-
-IF OBJECT_ID('tempdb..#Tmp') IS NOT NULL DROP TABLE #Tmp;
